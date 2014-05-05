a93af67b296ae1e287622c63b035ac6d8dbd3fa8
Randy Heiland <heiland@Randys-MacBook-Pro.local>
2014-05-04 20:48:54 -0400

    first commit

---
+++ showOBJ.py
@@ -0,0 +1,25 @@
+import sys
+from vtk import *
+#  python showOBJ.py average_lh_pial
+
+print 'len(sys.argv) =',len(sys.argv)
+if len(sys.argv) < 2:
+  print 'Usage: ' + argv[0] + 'filename (without suffix)'
+  sys.exit(1)
+fname = sys.argv[1]
+
+ren1 = vtkRenderer()
+renWin = vtkRenderWindow()
+renWin.AddRenderer(ren1)
+iren = vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+reader = vtkOBJReader()
+reader.SetFileName(fname + '.obj')
+mapper = vtkPolyDataMapper()
+mapper.SetInputConnection(reader.GetOutputPort())
+actor = vtkActor()
+actor.SetMapper(mapper)
+ren1.AddActor(actor)
+renWin.Render()
+iren.Initialize()
+iren.Start()
